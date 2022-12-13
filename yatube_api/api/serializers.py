88e510320a90b66ef9b1f8cc1459ce982805032d
murat <m.kaniukov@gmail.com>
2022-12-13 20:52:44 +0100

    v1

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from posts.models import Comment, Group, Post
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True,
+                                          slug_field='username')
+    group = serializers.SlugRelatedField(read_only=True, slug_field='title')
+
+    class Meta:
+        model = Post
+        fields = ('id', 'text', 'author', 'image', 'group', 'pub_date')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
---
+++ yatube_api/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import GroupViewSet, PostViewSet, CommentViewSet
+
+
+router = DefaultRouter()
+
+router.register('posts', PostViewSet, basename='posts')
+router.register('posts/(?P<post_id>\\d+)/comments', CommentViewSet,
+                basename='comments')
+router.register('groups', GroupViewSet, basename='groups')
+
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+    ]
