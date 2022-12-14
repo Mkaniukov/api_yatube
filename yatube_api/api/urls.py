6ae5ebb081721bfae615a58aad8da102037b9a9b
murat <m.kaniukov@gmail.com>
2022-12-14 09:53:10 +0100

    v1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import GroupViewSet, PostViewSet, CommentViewSet
+from .views import GroupViewSet, PostViewSet, CommentViewSet
 
 router = DefaultRouter()
 
